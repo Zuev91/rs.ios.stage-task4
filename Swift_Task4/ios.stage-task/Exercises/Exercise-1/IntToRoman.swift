b01e739ed7381b3659872a4de0583015d7d668a2
zuev91 <zionix007@gmail.com>
2021-06-07 23:35:58 +0300

    init: completed tasks

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,76 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        var number = self
+        var result = Array<String>()
+        var buffer = 0
+        if (1 ... 3999).contains(number) {
+                if (number / 1000 != 0 ) {
+                    buffer = number / 1000
+                    let i = Array(repeating: "M", count: buffer)
+                    result.append(contentsOf: i)
+                    number -= buffer * 1000
+                }
+                if (number / 100 != 0 ) {
+                    buffer = number / 100
+                    if buffer == 4 {
+                        result.append("CD")
+                        number -= buffer * 100
+                    } else if buffer == 9 {
+                        result.append("CM")
+                        number -= buffer * 100
+                    } else if buffer == 5 {
+                        result.append("D")
+                        number -= buffer * 100
+                    } else if (6 ... 8).contains(buffer) {
+                        result.append("D")
+                        let i = Array(repeating: "C", count: buffer - 5)
+                        result.append(contentsOf: i)
+                        number -= buffer * 100
+                    } else {
+                        let i = Array(repeating: "C", count: buffer)
+                        result.append(contentsOf: i)
+                        number -= buffer * 100
+                    }
+                }
+                if (number / 10 != 0 ) {
+                    buffer = number / 10
+                    if buffer == 4 {
+                        result.append("XL")
+                        number -= buffer * 10
+                    } else if buffer == 9 {
+                        result.append("XC")
+                        number -= buffer * 10
+                    } else if buffer == 5 {
+                        result.append("L")
+                        number -= buffer * 10
+                    } else if (6 ... 8).contains(buffer) {
+                        result.append("L")
+                        let i = Array(repeating: "X", count: buffer - 5)
+                        result.append(contentsOf: i)
+                        number -= buffer * 10
+                    } else {
+                        let i = Array(repeating: "X", count: buffer)
+                        result.append(contentsOf: i)
+                        number -= buffer * 10
+                    }
+                }
+                if (number != 0 ) {
+                    buffer = number
+                    switch buffer {
+                    case 1 ... 3: result.append(contentsOf: Array(repeating: "I", count: buffer))
+                    case 4: result.append("IV")
+                    case 5: result.append("V")
+                    case 6 ... 8: result.append("V")
+                        result.append(contentsOf: Array(repeating: "I", count: buffer - 5))
+                    case 9: result.append("IX")
+                    default:
+                        fatalError("number is not expected")
+                    }
+                }
+            let final = result.joined(separator:"")
+            return final
+        }
+        else { return nil }
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -2,7 +2,88 @@ import Foundation
 
 final class FillWithColor {
     
+    fileprivate func validate(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> Bool {
+        
+        if image.isEmpty {
+            return false
+        }
+        
+        if image[0].isEmpty {
+            return false
+        }
+        
+        if !(0...image.count).contains(row) {
+            return false
+        }
+        
+        if !(0...image[0].count).contains(column) {
+            return false
+        }
+        
+        for value in image {
+            for tmp in value {
+                if tmp < 0 {
+                    return false
+                }
+            }
+        }
+        
+        if newColor > 65536 {
+            return false
+        }
+        
+        return true
+    }
+    
+    static func neighbours(_ row:Int, _ column:Int, _ rowsCount:Int, _ columnsCount:Int) -> [(Int,Int)] {
+            var result:[(Int, Int)] = []
+            for (i,j) in [(row, column - 1), (row, column + 1), (row - 1, column), (row + 1, column)] {
+                    if (i >= 0  && j >= 0 && i < rowsCount && j < columnsCount) {
+                        result.append((i, j))
+                    }
+            }
+            return result
+        }
+    
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        if !validate(image, row, column, newColor) {
+            return image
+            }
+        let rowsCount = image.count
+        let columnsCount = image[0].count
+        var isVisited: [[Bool]] = [[]]
+        var mutableImage = image
+        let oldColor = image[row][column]
+
+        isVisited = Array(repeating: Array(repeating: false, count: columnsCount), count: rowsCount)
+        isVisited[row][column] = true
+        var peaks = [(row, column)]
+        mutableImage[row][column] = newColor
+        while peaks.count > 0 {
+        let (row, column) = peaks[peaks.count - 1]
+        peaks.remove(at:peaks.count - 1)
+        let neighbours = FillWithColor.neighbours(row, column, rowsCount, columnsCount)
+        for (i,j) in neighbours {
+            if !isVisited[i][j] && image[i][j] == oldColor {
+                isVisited[i][j] = true
+                mutableImage[i][j] = newColor
+                peaks.append((i,j))
+                }
+            }
+        }
+                return mutableImage
     }
 }
+
+//An image is represented by an `m = row x n = column` integer grid image where `image[i][j]` represents the pixel value of the image.
+//
+//In turn, three integers are given (`row, column, newColor`). It is necessary to fill the image with color starting from the pixel image - `image[row][column]`.
+
+
+//**`m == image.length`**
+//**`n == image[i].length`**
+//**`1 <= m, n <= 50`**
+//**`0 <= image[i][j], newColor < 65536`**
+//**`0 <= row <= m`**
+//**`0 <= column <= n`**
+
